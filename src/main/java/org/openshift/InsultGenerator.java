858df57169cc770acedcd7324687fdf65b8ddd3b
vganeshkumar <52894795+vganeshkumar@users.noreply.github.com>
2021-02-23 19:02:25 -0500

    Update InsultGenerator.java

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -1,6 +1,10 @@
 package org.openshift;
 
 import java.util.Random;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class InsultGenerator {
 	public String generateInsult() {
